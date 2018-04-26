268b742a13c1a54ac1e091ddc72036d2ebcf90fa
Dani Knoll <knoll.dani@gmail.com>
2018-04-26 17:58:44 +0200

    App info buttons JS implemented some loads static data

--- src/resources/public/js/dom.js
+++ src/resources/public/js/dom.js
@@ -10,13 +10,14 @@ dom = {
         delay: 500,
         interrupted: false,
         reverseOrder: false,
+        infoData: "",
     },
 
     initFunctions: {
 
         saveDemoArea: function () {
             dom.initFunctions.createGrid();
-            $(".gridGenButtons").hide();
+            $(".infoBtnsGrid").hide();
             $(".playerBtns").hide();
             return $("section.demoArea").html();
         },
@@ -39,6 +40,7 @@ dom = {
             dom.eventListeners.fastForwardMazeGenEventListener();
             dom.eventListeners.jumpToEndEventListener();
             dom.eventListeners.newMazeEventListener();
+            dom.eventListeners.infoBtnsEventListener();
         },
     },
 
@@ -47,7 +49,7 @@ dom = {
         navigationEventListener: function () {
             $("nav li").click(function () {
                 dom.data.interrupted = true;
-                $(".gridGenButtons").hide();
+                $(".infoBtnsGrid").hide();
                 $("nav li").removeClass("activeNav");
                 let selectedMenu = $(this);
                 selectedMenu.addClass("activeNav");
@@ -69,8 +71,8 @@ dom = {
             $("#generate").click(function () {
                 dom.dataFunctions.resetIterDelayOrder();
                 $(this).hide();
-                $(".playerBtns").show(100);
-                $(".gridGenButtons").show(200);  // Todo another animation
+                $(".playerBtns").slideDown(100);
+                $(".infoBtnsGrid").slideDown(200);
                 dom.utility.mazeGeneration();
             });
         },
@@ -164,6 +166,42 @@ dom = {
                 dom.utility.changePlayToPause();
                 dom.utility.mazeGeneration();
             });
+        },
+
+        infoBtnsEventListener: function () {
+            /**
+             * Info Area toggle.
+             * If you click a button and it is not active there is an active button, takes away class.
+             * If the area is not empty it slides up. And after the animation is done clears area.
+             * If you click a button which has info and it is active then it removes class
+             * otherwise it becomes active button and loads info with animation.
+             */
+            let showInfoBtns = ["showApi", "showCode", "showInfo"];
+            let infoSection = $(".appInfoSection");
+            let speed = 400;
+            $(".infoBtnsGrid > div").click(function () {
+                let clickedBtn = $(this);
+                if( !clickedBtn.hasClass("activeInfoBtn") && $(".infoBtnsGrid > div").hasClass("activeInfoBtn") ){
+                    $(".infoBtnsGrid  div.activeInfoBtn").removeClass("activeInfoBtn");
+                }
+                if(infoSection.html().toString().length > 0) {
+                    infoSection.slideUp(speed);
+                    setTimeout(function (){
+                        infoSection.html("");
+                    }, speed);
+                }
+                if(showInfoBtns.includes(clickedBtn.attr("id"))) {
+                    if (clickedBtn.hasClass("activeInfoBtn")) {
+                        clickedBtn.removeClass("activeInfoBtn");
+                    } else {
+                        clickedBtn.addClass("activeInfoBtn");
+                        setTimeout(function () {
+                            dom.utility.loadInfo(clickedBtn.attr("id"));
+                        }, speed);
+                        infoSection.slideDown(speed);
+                    }
+                }
+            });
         }
     },
 
@@ -248,6 +286,22 @@ dom = {
             if(iterator < length) {
                 $(".maze div:nth-child(" + order[iterator] + ")").removeClass("mazeGenPointer").addClass("mazeWall");
             }
+        },
+
+        loadInfo: function(btnId) {
+            let infoSection = $(".appInfoSection");
+            switch (btnId) {
+                case "showApi":
+                    infoSection.html(dom.htmlStructures.apiInfo);
+                    break;
+                case "showCode":
+                    infoSection.html(dom.htmlStructures.codeInfo);
+                    break;
+                case "showInfo":
+                    infoSection.html(dom.htmlStructures.algoInfo);
+                    break;
+                default:
+            }
         }
     },
 
@@ -265,4 +319,66 @@ dom = {
             dom.data.reverseOrder = false;
         }
     },
+
+    htmlStructures: {
+
+        apiInfo: `
+                <h3 class="title">API Info</h3>
+                    <div class="snippet">
+                        <code>http://www.future-domain-name.hu/api/generate?<span class="apiValues"></span></code>
+                    </div>
+                    <div class="apiInfo">
+                        <ol>
+                            <li>input: 0: thick wall / 1: thin wall</li>
+                            <li>input: 0-4 generation algorithms</li>
+                            <li>input: maze width (3-100)</li>
+                            <li>input: maze height (3-100)</li>
+                        </ol>
+                        <div class="form">
+                            <form>
+                                <select id="wallType" name="wall">
+                                    <option value="0">thick wall</option>
+                                    <option value="1">thin wall</option>
+                                </select>
+                                <select id="algoType" name="algo">
+                                    <option value="0">DFS</option>
+                                    <option value="1">Kruskal</option>
+                                    <option value="1">Random</option>
+                                </select>
+                                <input type="number" value="3" max="100"/>
+                                <input type="number" value="3" max="100"/>
+                                <input class="btn singleBtn formSubmit" type="submit" value="Send"/>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="snippet">
+                        <code class="json"></code>
+                    </div>
+        `,
+
+        algoInfo: `
+                <h3 class="title">Algorithm Info</h3>
+                <div class="algoInfo">
+                    <article>
+                        <h4 class="title">Depth-first search algorithm</h4>
+                        <p>Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures. One starts at the root (selecting some arbitrary node as the root in the case of a graph) and explores as far as possible along each branch before backtracking.</p>
+                        <p>A version of depth-first search was investigated in the 19th century by French mathematician Charles Pierre Trémaux[1] as a strategy for solving mazes.</p>
+                        <p>Nunc condimentum, nulla in faucibus commodo, elit massa pharetra nibh, at tincidunt libero dolor quis augue. Donec pulvinar consectetur tortor, eget gravida ligula molestie at. Aenean ullamcorper tempor fermentum. Vestibulum metus ante, aliquam sed ligula vitae, auctor tempus quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae imperdiet justo, sed egestas nulla. Nunc sit amet malesuada mauris, sed pellentesque arcu. Sed venenatis mattis mi ac tincidunt. Pellentesque nec justo ullamcorper, cursus massa at, condimentum justo. Mauris pharetra ligula in nibh efficitur, ut facilisis nisi pulvinar. Nullam eu consectetur mi. In in dapibus sem, a pharetra sem. Pellentesque in blandit magna. Nulla in orci finibus, ornare massa ut, mollis velit.</p>
+                        <p>Etiam condimentum congue est, eget accumsan enim ornare eget. Morbi sed dui sit amet purus hendrerit egestas. Mauris at tellus sit amet dolor commodo suscipit sit amet eget elit. Curabitur nec diam id risus pulvinar euismod. Sed et rutrum lacus. Fusce sit amet augue auctor nulla semper luctus eget at dolor. Vivamus egestas tincidunt tincidunt. Donec sapien velit, venenatis eu tincidunt nec, consequat eu lacus. Nulla finibus sodales mauris, sed consequat urna hendrerit sed. Morbi eleifend consectetur imperdiet. Morbi eu venenatis mauris. Morbi non mi vel orci ultricies semper. Pellentesque rutrum, odio sit amet auctor accumsan, enim arcu vestibulum quam, vel fringilla leo odio eget lorem. Sed nisl purus, porttitor et sagittis vel, pharetra lacinia lectus.</p>
+                    </article>
+                    <aside class="images">
+                        <h4 class="title">Images:</h4>
+                        <img src="img/300px-Depth-first-tree.svg.png"/>
+                        <img src="img/300px-Depth-first-tree.svg.png"/>
+                    </aside>
+                </div>
+        `,
+
+        codeInfo: `
+                <h3 class="title">Class Name</h3>
+                    <div class="snippet">
+                        <pre><code class="algoCode">code will be here</code></pre>
+                    </div>
+        `
+    }
 };
