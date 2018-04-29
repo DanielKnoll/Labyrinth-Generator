a78143e96f5056d8ef589bec488834ed10aa7abb
Dani Knoll <knoll.dani@gmail.com>
2018-04-30 01:25:17 +0200

    Info end point

--- src/java/com.codecool.labyrinth_generator/api/LabyrinthServiceREST.java
+++ src/java/com.codecool.labyrinth_generator/api/LabyrinthServiceREST.java
@@ -1,6 +1,8 @@
 package com.codecool.labyrinth_generator.api;
 
 import com.codecool.labyrinth_generator.factory.LabyrinthGeneratorFactory;
+import com.codecool.labyrinth_generator.Information.AlgorithmInfo;
+import com.codecool.labyrinth_generator.factory.LabyrinthInfoFactory;
 import com.codecool.labyrinth_generator.generator.Labyrinth;
 import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
@@ -21,10 +23,31 @@ public class LabyrinthServiceREST {
 
     @PostMapping(value = "/api/generate")
     public ResponseEntity getAllMazeDataPost(@RequestBody Map<String, String> data) {
-        String typeString = data.get("algoType").toString();
-        String widthString = data.get("mazeColNum").toString();
-        String heightString = data.get("mazeRowNum").toString();
+        String typeString = data.get("algoType");
+        String widthString = data.get("mazeColNum");
+        String heightString = data.get("mazeRowNum");
         return generateLabyrinth(typeString, widthString, heightString);
+    }
+
+    @PostMapping(value = "/api/info")
+    public ResponseEntity getAlgoInfoPost(@RequestBody Map<String, String> data) {
+        int type;
+        String typeString = data.get("algoType");
+        try {
+            type = Integer.parseInt(typeString);
+        } catch (NumberFormatException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        LabyrinthInfoFactory factory = new LabyrinthInfoFactory();
+        AlgorithmInfo algorithm = factory.getInfo(type);
+
+        if(algorithm == null){
+            return new ResponseEntity("Wrong type. (0-8)", HttpStatus.BAD_REQUEST);
+        }
+
+        CreateJson createJson = new CreateJson();
+        JSONObject jsonObject = createJson.getInfoJson(algorithm);
+        return new ResponseEntity(jsonObject, HttpStatus.OK);
     }
 
     private ResponseEntity generateLabyrinth(String typeString, String widthString, String heightString) {
@@ -45,7 +68,7 @@ public class LabyrinthServiceREST {
 
         if(labyrinth != null) {
             CreateJson createJson = new CreateJson();
-            JSONObject jsonObject = createJson.getJson(labyrinth);
+            JSONObject jsonObject = createJson.getLabyrinthJson(labyrinth);
             return new ResponseEntity(jsonObject, HttpStatus.OK);
         } else {
             return new ResponseEntity("Wrong input", HttpStatus.BAD_REQUEST);
