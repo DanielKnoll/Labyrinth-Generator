7bb94570d696c76cae3b26dcf2b1f2856e724dfa
Dani Knoll <knoll.dani@gmail.com>
2018-05-02 14:33:03 +0200

    When first time reaches the wall across creates an end tile.

--- src/java/com.codecool.labyrinth_generator/generator/Dfs.java
+++ src/java/com.codecool.labyrinth_generator/generator/Dfs.java
@@ -6,7 +6,8 @@ import java.util.List;
 import java.util.Stack;
 
 public class Dfs extends Labyrinth {
-    Stack<Node> stack = new Stack<>();
+    private Stack<Node> stack = new Stack<>();
+    private boolean isEndTileFound = false;
 
     public Dfs(int mazeWidth, int mazeHeight) {
         algoName = "Depth-first search algorithm";
@@ -36,6 +37,7 @@ public class Dfs extends Labyrinth {
 
             nextPlace = nextTile.getPlace();
             setMazeTileCorridor(nextPlace[0], nextPlace[1]);
+            setEndTile(nextPlace[0], nextPlace[1]);
             stack.push(nextTile);
             createCorridor(currentTile, nextTile);
             generateLabyrinth(nextPlace);
@@ -77,7 +79,24 @@ public class Dfs extends Labyrinth {
         }
     }
 
-    /*public void generateLabyrinth() {
-        this.mazeOrder = Arrays.asList(166, 148, 147, 129, 111, 112, 94, 76, 58, 40, 41, 42, 43, 61, 79, 78, 96, 114, 132, 150, 151, 152, 153, 154, 136, 118, 119, 120, 121, 103, 85, 86, 68, 69, 70, 71, 146, 145, 127, 109, 91, 75, 74, 56, 55, 37, 19, 20, 21, 23, 25, 26, 27, 45, 63, 81, 99, 98, 116, 28, 82, 83, 65, 47, 48, 49, 31, 32, 33, 34, 155, 156, 157, 158, 159, 160, 142, 124, 106, 105);
-    }*/
+    private void setEndTile(int x, int y) {
+        int[] curentTile = new int[]{x, y};
+        int[] startTile = mazeOrder2D.get(0);
+
+        if(mazeOrder.size() > 1 && !isEndTileFound) {
+            if (startTile[0] == 0 && curentTile[0] + 1 == mazeHeight - 1) {
+                setMazeTileCorridor(curentTile[0] + 1, curentTile[1]);
+                isEndTileFound = true;
+            } else if (startTile[0] == mazeHeight && curentTile[0] - 1 == 0) {
+                setMazeTileCorridor(curentTile[0] - 1, curentTile[1]);
+                isEndTileFound = true;
+            } else if (startTile[1] == 0 && curentTile[1] + 1 == mazeWidth - 1) {
+                setMazeTileCorridor(curentTile[0], curentTile[1] + 1);
+                isEndTileFound = true;
+            } else if (startTile[1] == mazeWidth && curentTile[1] - 1 == 0) {
+                setMazeTileCorridor(curentTile[0], curentTile[1] - 1);
+                isEndTileFound = true;
+            }
+        }
+    }
 }
